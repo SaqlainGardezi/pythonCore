c2451cea2ff2f16df717f20c898d6d41616e3d7b
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2018-11-05 17:02:35 +0000

    Boolean

--- numerictypes.py
+++ numerictypes.py
@@ -18,6 +18,20 @@ bin=0b10101
 hexa=0Xfa18
 octal=0o745
 
+""" all have type of int when printed"""
+
 print(bin, " has type ",type(bin))
 print(hexa, " has type ", type(hexa))
 print(octal , " has type ", type(octal))
+
+
+#Boolean capital T,F in True,False
+boolT=True
+boolF=False
+print(boolT, " has type ", type(boolT))
+print(boolF, ' has type ', type(boolF))
+
+print(5<4)
+a=43
+x=43
+print(a<=x)

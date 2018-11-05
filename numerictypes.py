7a6e5c720694f7403238951cd4f440c2d2977ff0
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2018-11-05 16:40:03 +0000

    complex, binary hexadecimal and octal types

--- numerictypes.py
+++ numerictypes.py
@@ -7,3 +7,17 @@ x=33.5
 y=-54.65
 print("Saqlain")
 print(type(y))
+
+#complex type
+d=3+5j
+print(d)
+print type(d)
+
+#binary and hexadecimal types
+bin=0b10101
+hexa=0Xfa18
+octal=0o745
+
+print(bin, " has type ",type(bin))
+print(hexa, " has type ", type(hexa))
+print(octal , " has type ", type(octal))
